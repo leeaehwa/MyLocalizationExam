4351b5ed5ca4e53acf799f77830af3e6775e5ae1
leeaehwa <aehwa830@helloworlds.co.kr>
2016-07-29 17:46:40 +0900

    프로젝트 업로

---
+++ MyLocalizationExam/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace MyLocalizationExam
+{
+    class MainViewModel
+    {
+        private int _language;
+        public int Language 
+        {
+            get { return _language; }
+            set
+            {
+                //언어 설정 적용시키는 작업을 해야한다.
+                _language = value;
+                if (value == 0)
+                    Properties.Settings.Default.Locale = "ko-KR";
+                else
+                    Properties.Settings.Default.Locale = "en-US";
+                Properties.Settings.Default.Save();
+
+                SelectCulture(Properties.Settings.Default.Locale);
+            }
+        }
+
+        public static void SelectCulture(string culture)
+        {
+            // List all our resources       
+            List<ResourceDictionary> dictionaryList = new List<ResourceDictionary>();
+            foreach (ResourceDictionary dictionary in Application.Current.Resources.MergedDictionaries)
+            {
+                dictionaryList.Add(dictionary);
+            }
+            // We want our specific culture       
+            string requestedCulture = string.Format("/Assets/StringResources.{0}.xaml", culture);
+            ResourceDictionary resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            if (resourceDictionary == null)
+            {
+                //리소스를 찾을수 없다면 기본 리소스로 지정      
+                requestedCulture = "/Assets/StringResources.ko-KR.xaml";
+                resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            }
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+
+            //지역화 설정
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+
+
+
+
+
+            //윈도우 새로고침같은걸 어캐하지
+
+        
+        
+        }
+    }
+}
---
+++ MyLocalizationExam/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLocalizationExam
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            /**
+             * 참고 http://kojaedoo.tistory.com/648
+             */
+
+//            Properties.Settings.Default.Locale = "ko-KR";
+
+//            SelectCulture("ko-KR");
+//            SelectCulture("en-US");
+            SelectCulture(Properties.Settings.Default.Locale);
+
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        public static void SelectCulture(string culture)
+        {
+            // List all our resources       
+            List<ResourceDictionary> dictionaryList = new List<ResourceDictionary>();
+            foreach (ResourceDictionary dictionary in Application.Current.Resources.MergedDictionaries)
+            {
+                dictionaryList.Add(dictionary);
+            }
+            // We want our specific culture       
+            string requestedCulture = string.Format("/Assets/StringResources.{0}.xaml", culture);
+            ResourceDictionary resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            if (resourceDictionary == null)
+            {
+                //리소스를 찾을수 없다면 기본 리소스로 지정      
+                requestedCulture = "/Assets/StringResources.ko-KR.xaml";
+                resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            }
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+
+            //지역화 설정
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+        }
+    }
+
+
+}
